4a34ee07dea0ece734467237ef535c8404636fbe
HenryFake <henry.faick69@gmail.com>
2023-07-12 18:59:50 -0500

    Ordenando los .dart y separandolo por carpeta

---
+++ lib/screens/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:see_signal/screens/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vision Auditiva',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Vision Auditiva'),
+    );
+  }
+}
